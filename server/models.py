b1a687f83a3ec4a5a75562a976f8bfbebff3ca11
Molly Todd <mollyjt89@gmail.com>
2023-10-16 10:35:14 -0700

    taco model done

--- server/models.py
+++ server/models.py
@@ -9,10 +9,45 @@ class User(db.Model, SerializerMixin): # Attributes will be user_id, username, i
     pass
      
 class Taco(db.Model, SerializerMixin): # Attributes will be taco_id, name, recipe
-    pass
+    __tablename__ = 'taco'
+    serialize_rules = (
+        "-favorites.taco",
+        "-user.tacos",
+        "-quantities.taco",
+        "-reviews.taco"
+
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    taco_name = db.Column(db.String)
+    image = db.Column(db.String)
+    instructions = db.Column(db.Text)
+    time_to_cook = db.Column(db.Integer)
+    time_to_prepare = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+     # table relationships
+    reviews = db.relatinoship("Review", backref="taco", cascade ="all, delete-orphan")
+    quantities = db.relationship("Quantity", backref="taco", cascade ="all, delete-orphan")
+
+    # association proxy
+
+    ingredients = association_proxy ("quantities", "ingredient")
+
+    def __repr__ (self):
+        return f"Taco {self.taco_name} ID {self.id}"
+    
+    #add validation rules here once seeded
+    
 
 class Review(db.Model, SerializerMixin): # Attributes will be review_id, rating, comment
     pass
  
-class UserTacoAssociation(db.Model, SerializerMixin): # Attribute will be association_id, user_id, taco_id
+
+class Quantity(db.Model, SerializerMixin): # Attributes will b
+    pass
+
+class Ingredients(db.Model, SerializerMixin): # Attributes will
+    pass
+
+class Favorites (db.Model, SerializerMixin): # Attributes will
     pass
