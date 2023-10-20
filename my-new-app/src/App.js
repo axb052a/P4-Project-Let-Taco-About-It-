032eadc723b16cd12b7e7aa9358019d2d3a95972
mollytodd <131923397+mollytodd@users.noreply.github.com>
2023-10-20 09:29:28 -0700

    Update App.js

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -4,26 +4,47 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./Home";
 import Login from "./Login"; // Import your Login component
 import NavBar from "./NavBar";
+import Signup from "./Signup"; // Import your Login component
+
+
+import AddTaco from "./AddTaco";
 
 
 function App() {
 
   const [user, setUser] = useState(null);
-  return (
-    <ChakraProvider>
-      <Router>
-        <NavBar /> {NavBar}
-        <Switch>
-          <Route path="/login">
-            <Login user={user} setUser={setUser} />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </ChakraProvider>
-  );
+
+  const fetchUser = () =>
+    fetch("http://localhost:5555/check_session").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setUser(user);
+          console.log(user);
+        });
+      }
+    });
+
+return (
+  <ChakraProvider>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/login">
+          <Login user={user} setUser={setUser} fetchUser={fetchUser} />
+        </Route>
+        <Route path="/signup">
+          <Signup user={user} setUser={setUser} fetchUser={fetchUser} />
+        </Route>
+        <Route path="/AddTaco">
+          <AddTaco  />
+        </Route>
+        <Route path="/">
+          <Home user={user} />
+        </Route>
+      </Switch>
+    </Router>
+  </ChakraProvider>
+);
 }
 
 export default App;
