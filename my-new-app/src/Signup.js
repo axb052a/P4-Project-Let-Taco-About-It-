3fcae9efe74545389e5568b8cc5fc1d480bf69be
axb052a <119557887+axb052a@users.noreply.github.com>
2023-10-20 09:25:22 -0700

    Signup is showing.

--- my-new-app/src/Signup.js
+++ my-new-app/src/Signup.js
@@ -9,21 +9,16 @@ import {
   Input as ChakraInput,
   Button,
   SimpleGrid,
- 
-  useBreakpointValue,
-
-  Icon,
 } from "@chakra-ui/react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 
-export default function Signup({ user, setUser, fetchUser }) {
+export default function Signup({ setUser, fetchUser }) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
-
   const history = useHistory();
 
-  function handleLogin(e) {
+  function handleLogin() {
     history.push("/login");
   }
 
@@ -37,36 +32,14 @@ export default function Signup({ user, setUser, fetchUser }) {
       body: JSON.stringify({ username, email, password }),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((user) => setUser(user));
-        fetchUser(user);
-
-        history.push("/");
+        r.json().then((user) => {
+          setUser(user);
+          fetchUser(user);
+          history.push("/");
+        });
       }
     });
   }
-
-  // const Blur = (props: IconProps) => {
-  const Blur = (props) => {
-    return (
-      <Icon
-        width={useBreakpointValue({ base: "100%", md: "40vw", lg: "30vw" })}
-        zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
-        height="560px"
-        viewBox="0 0 528 560"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-      >
-        <circle cx="71" cy="61" r="111" fill="#F56565" />
-        <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-        <circle cy="291" r="139" fill="#ED64A6" />
-        <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-        <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-        <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-        <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
-      </Icon>
-    );
-  };
 
   return (
     <Box position={"relative"}>
@@ -85,15 +58,15 @@ export default function Signup({ user, setUser, fetchUser }) {
             Welcome to{" "}
             <Text
               as={"span"}
-              bgGradient="linear(to-r, red.400,pink.400)"
+              bgGradient="linear(to-r, yellow.400, orange.400)"
               bgClip="text"
             >
-              Noodle-On-It
+              Let's Taco About It
             </Text>{" "}
           </Heading>
         </Stack>
         <Stack
-          bg={"gray.50"}
+          bg={"yellow.50"}
           rounded={"xl"}
           p={{ base: 4, sm: 6, md: 8 }}
           spacing={{ base: 8 }}
@@ -101,33 +74,28 @@ export default function Signup({ user, setUser, fetchUser }) {
         >
           <Stack spacing={4}>
             <Heading
-              color={"gray.800"}
+              color={"orange.800"}
               lineHeight={1.1}
               fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
             >
-              Join the Noodle Community
+              Come on , lets Taco Bout' it! 
               <Text
                 as={"span"}
-                bgGradient="linear(to-r, red.400,pink.400)"
+                bgGradient="linear(to-r, yellow.400, orange.400)"
                 bgClip="text"
               >
                 !
               </Text>
             </Heading>
-            {/* <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
-              We’re looking for amazing engineers just like you! Become a part
-              of our rockstar engineering team and skyrocket your career!
-            </Text> */}
           </Stack>
           <Box as={"form"} onSubmit={(e) => handleSubmit(e)} mt={10}>
-            {/* <form onSubmit={(e) => handleSubmit(e)}> */}
             <Stack spacing={4}>
               <ChakraInput
-                bg={"gray.100"}
+                bg={"yellow.100"}
                 border={0}
-                color={"gray.500"}
+                color={"orange.500"}
                 _placeholder={{
-                  color: "gray.500",
+                  color: "orange.500",
                 }}
                 type="text"
                 value={username}
@@ -136,11 +104,11 @@ export default function Signup({ user, setUser, fetchUser }) {
                 required
               />
               <ChakraInput
-                bg={"gray.100"}
+                bg={"yellow.100"}
                 border={0}
-                color={"gray.500"}
+                color={"orange.500"}
                 _placeholder={{
-                  color: "gray.500",
+                  color: "orange.500",
                 }}
                 type="password"
                 value={password}
@@ -149,11 +117,11 @@ export default function Signup({ user, setUser, fetchUser }) {
                 required
               />
               <ChakraInput
-                bg={"gray.100"}
+                bg={"yellow.100"}
                 border={0}
-                color={"gray.500"}
+                color={"orange.500"}
                 _placeholder={{
-                  color: "gray.500",
+                  color: "orange.500",
                 }}
                 type="email"
                 value={email}
@@ -161,30 +129,21 @@ export default function Signup({ user, setUser, fetchUser }) {
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
-              {/* <Button
-                  fontFamily={"heading"}
-                  bg={"gray.200"}
-                  color={"gray.800"}
-                  type="submit"
-                >
-                  Join
-                </Button> */}
             </Stack>
             <Button
               fontFamily={"heading"}
               mt={8}
               w={"full"}
-              bgGradient="linear(to-r, red.400,pink.400)"
+              bgGradient="linear(to-r, yellow.400, orange.400)"
               color={"white"}
               _hover={{
-                bgGradient: "linear(to-r, red.400,pink.400)",
+                bgGradient: "linear(to-r, yellow.400, orange.400)",
                 boxShadow: "xl",
               }}
               type="submit"
             >
               Submit
             </Button>
-            {/* </form> */}
             <Text
               color={"blue.400"}
               _hover={{
@@ -195,17 +154,8 @@ export default function Signup({ user, setUser, fetchUser }) {
               Already have an account? Login now!
             </Text>
           </Box>
-          form
         </Stack>
       </Container>
-      <Blur
-        position={"absolute"}
-        top={-10}
-        left={-10}
-        style={{ filter: "blur(70px)" }}
-      />
     </Box>
   );
 }
-
-
