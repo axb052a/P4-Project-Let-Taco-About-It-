d5c42ce218168ebb597d1dbffdf3f992997c0a46
mollytodd <131923397+mollytodd@users.noreply.github.com>
2023-10-26 11:37:07 -0700

    Update config.py

--- server/config.py
+++ server/config.py
@@ -16,7 +16,7 @@ from flask_bcrypt import Bcrypt
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.secret_key = a'Z\cb1Wn\v00\xder|eQ\x60t \cca\w2s\c10KP'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
